492d22432f05f3078e1a7250a7fd4151940e52fd
MicheleAlbanese <michele.albanese93@gmail.com>
2016-11-21 17:03:33 +0100

    skeleton

---
+++ Microgue/Assets/Skeleton/Scripts/GameplayManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using POLIMIGameCollective;
+
+public class GameplayManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+	}
+}
