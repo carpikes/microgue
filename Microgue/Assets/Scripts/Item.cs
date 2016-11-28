b7cd6ca433dd2a717007f6ad357e2e348cf7af18
MicheleAlbanese <michele.albanese93@gmail.com>
2016-11-28 21:28:26 +0100

    boh

---
+++ Microgue/Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item {
+
+    public enum ItemCategories
+    {
+        Hearts
+    }
+
+    public string name;
+    public ItemCategories category;
+    public string stat1;
+    public string effect1;
+    public string stat2;
+    public string effect2;
+}
