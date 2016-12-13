d303fe9eff1283d65e36cbfaf00fbed32df42f70
Alain <alain.carlucci@mail.polimi.it>
2016-12-13 01:21:11 +0100

    jimmy demo

--- Microgue/Assets/Scripts/Enemies/LittleJimmy.cs
+++ Microgue/Assets/Scripts/Enemies/LittleJimmy.cs
@@ -24,6 +24,7 @@ public class LittleJimmy : MonoBehaviour
         mAIMap = GameObject.Find("GameplayManager").GetComponent<AIMap>();
 		mEnemyAI = GetComponent<EnemyAI>();
 		mEnemyAI.SetEnabled(true);
+        dist = Random.Range(2.0f, 3.0f);
     }
 
     float wait = 0;
@@ -33,6 +34,39 @@ public class LittleJimmy : MonoBehaviour
         return mAwayMap;
     }
 
+    IntPoint lastTile;
+    bool ltset = false;
+
+    float timeout = 0;
+    float dist;
+    float tget;
+    float newtget;
+    Vector2 mSpeed = Vector2.zero;
+    void FixedUpdate()
+    {
+        if (Time.time > timeout) {
+            timeout = Time.time + Random.Range(2, 3);
+            newtget = Random.Range(0, 2.0f * Mathf.PI);
+        }
+
+        if (newtget != tget)
+            tget = Mathf.LerpAngle(tget, newtget, Time.fixedDeltaTime);
+
+        Vector2 delta = mTarget.transform.position - transform.position;
+        delta += dist * new Vector2(Mathf.Cos(tget), Mathf.Sin(tget));
+
+        float dl = delta.magnitude;
+        delta.Normalize();
+
+        mSpeed += delta * dl * 10.0f * Time.fixedDeltaTime;
+        mSpeed *= 0.95f;
+
+        Vector2 t = transform.position;
+        t += mSpeed * Time.fixedDeltaTime;
+        transform.position = t;
+
+    }
+    /*
     void Update()
     {
         transform.localScale = new Vector3(mRb.position.x >= mPlayerRb.position.x ? 1 : -1, 1, 1);
@@ -50,26 +84,32 @@ public class LittleJimmy : MonoBehaviour
         {
             int w = mAIMap.GetWidth();
             int h = mAIMap.GetHeight();
-            IntPoint p = mAIMap.GetPosition(mRb.position);
+            if (!ltset)
+            {
+                lastTile = mAIMap.GetPosition(mRb.position);
+                ltset = true;
+            }
 
-            int q = mAwayMap[p.x + w * p.y];
-            IntPoint where = p;
+            int q = mAwayMap[lastTile.x + w * lastTile.y];
+            IntPoint where = lastTile;
 
+            IntPoint p = lastTile;
             if (p.x < w-1 && mAwayMap[p.x + 1 + w * p.y]   <= q && mObstacleMap[p.x + 1 + w * p.y]   == 0) { q = mAwayMap[p.x + 1 + w * p.y]; where = new IntPoint(p.x + 1, p.y); }
             if (p.x > 0   && mAwayMap[p.x - 1 + w * p.y]   <= q && mObstacleMap[p.x - 1 + w * p.y]   == 0) { q = mAwayMap[p.x - 1 + w * p.y]; where = new IntPoint(p.x - 1, p.y); }
             if (p.y < h-1 && mAwayMap[p.x + w * (p.y + 1)] <= q && mObstacleMap[p.x + w * (p.y + 1)] == 0) { q = mAwayMap[p.x + w * (p.y+1)]; where = new IntPoint(p.x, p.y+1); }
             if (p.y > 0   && mAwayMap[p.x + w * (p.y - 1)] <= q && mObstacleMap[p.x + w * (p.y - 1)] == 0) { q = mAwayMap[p.x + w * (p.y-1)]; where = new IntPoint(p.x, p.y-1); }
 
-            Debug.Log("Passo dal tile" + p.x + "," + p.y + " al " + where.x + "," + where.y);
+            //Debug.Log("Passo dal tile" + p.x + "," + p.y + " al " + where.x + "," + where.y);
 
             Vector2 whereWS = mAIMap.GetWorldPosition(where);
             Vector2 dir = (whereWS - mRb.position);
 
-            if (p.x != where.x || p.y != where.y)
+            if (lastTile.x != where.x || lastTile.y != where.y)
             {
-                Vector2 pos = transform.position;
-                pos += dir.normalized * 3.0f * Time.fixedDeltaTime;
-                mRb.position = pos;
+                //Vector2 pos = transform.position;
+                //pos += dir.normalized * 3.0f * Time.deltaTime;
+                mRb.position = mAIMap.GetWorldPosition(where);
+                lastTile = where;
             }
         }
     }
@@ -116,4 +156,6 @@ public class LittleJimmy : MonoBehaviour
         }
         Debug.Log(str);
     }
+
+    */
 }
