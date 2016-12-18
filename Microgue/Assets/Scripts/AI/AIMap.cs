08aac66a5d2ac9deeaf827fa43e70677677ec4ff
MicheleAlbanese <michele.albanese93@gmail.com>
2016-12-18 18:20:32 +0100

    working on wc/tile coords fix

--- Microgue/Assets/Scripts/AI/AIMap.cs
+++ Microgue/Assets/Scripts/AI/AIMap.cs
@@ -8,19 +8,26 @@ public class AIMap : MonoBehaviour
     GameObject mPlayer = null;
     byte[] mArea, mEnemies;
     int mMapRefreshes;
-    int mWidth, mHeight;
+    int mRowTiles, mColTiles;
+    float mMapWidthWC, mMapHeightWC;
+    float mTileWidthWC, mTileHeightWC;
     Bounds mWorldArea;
 	private float mEnemyUpdateInterval = 0.1f;
 	private float mNextEnemyUpdate = 0.0f;
 
 	public struct IntPoint 
 	{ 
-		public int x, y; 
-		public IntPoint(int x, int y)
-		{
-			this.x = x;
-			this.y = y;
-		}
+		public int x, y;
+        public IntPoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", x, y);
+        }
 	};
 
     public IntPoint GetPlayerPosition()
@@ -30,6 +37,7 @@ public class AIMap : MonoBehaviour
 
     public IntPoint GetTilePosition(Vector2 pos)
     {
+        Debug.Log(pos + ", " + WorldToTileCoordinates(pos).x + "-" + WorldToTileCoordinates(pos).y + ", "); // + TileToWorldCoordinates(WorldToTileCoordinates(pos)));
         return WorldToTileCoordinates(pos);
     }
 
@@ -38,40 +46,38 @@ public class AIMap : MonoBehaviour
         return TileToWorldCoordinates(pos);
     }
 
-    public byte[] GetMap()
-    {
-        return mArea;
-    }
-
-    public byte[] GetEnemies()
-    {
-        return mEnemies;
-    }
-
-    public int GetMapRefreshId()
-    {
-        return mMapRefreshes;
-    }
-
-    public int GetWidth()
-    {
-        return mWidth;
-    }
-
-    public int GetHeight()
-    {
-        return mHeight;
-    }
-
 	// Use this for initialization
 	void Start () {
         mGameManager = GameObject.Find("GameplayManager").GetComponent<GameplayManager>();
         mWorld = null;
         mPlayer = GameObject.Find("MainCharacter");
-	}
-		
-	// Update is called once per frame
-	void Update () {
+    }
+
+    private void InitializeMapMeasures(Tiled2Unity.TiledMap map)
+    {
+        mRowTiles = map.NumTilesWide;
+        mColTiles = map.NumTilesHigh;
+
+        // 32 x 24 ok!
+        //Debug.Log("DIMENSIONI MAPPA: " + mRowTiles + " " + mColTiles);
+
+        mMapWidthWC = map.GetMapWidthInPixelsScaled();
+        mMapHeightWC = map.GetMapHeightInPixelsScaled();
+
+        // 10.24 x 7.68 ok!
+        // Debug.Log("MAPPA IN WC " + mMapWidthWC + " , " + mMapHeightWC);
+
+        mTileWidthWC = map.TileWidth * map.ExportScale;
+        mTileHeightWC = map.TileHeight * map.ExportScale;
+
+        // 0.32 0.32 ok!
+        // Debug.Log("DIMENSIONI TILE IN WC: " + mTileWidthWC + " " + mTileHeightWC);
+
+        mWorldArea = new Bounds(new Vector2(mMapWidthWC / 2, mMapHeightWC / 2), new Vector2(mMapWidthWC, mMapHeightWC));
+    }
+
+    // Update is called once per frame
+    void Update () {
         GameObject world = mGameManager.GetWorld();
 
         // TODO EVENTO!!
@@ -80,9 +86,9 @@ public class AIMap : MonoBehaviour
             mWorld = world;
             UpdateArea();
         }
-		if (mWorld != null && mWidth > 0 && mHeight > 0)
+		if (mWorld != null && mRowTiles > 0 && mColTiles > 0)
 		{
-			if (mNextEnemyUpdate < Time.time)
+            if (mNextEnemyUpdate < Time.time)
 			{
 				UpdateEnemies();
 				mNextEnemyUpdate = Time.time + mEnemyUpdateInterval;
@@ -112,29 +118,28 @@ public class AIMap : MonoBehaviour
         EdgeCollider2D[] coll = GameObject.Find(mWorld.name + "/Collision").GetComponentsInChildren<EdgeCollider2D>();
         PolygonCollider2D[] poly = GameObject.Find(mWorld.name + "/Collision").GetComponentsInChildren<PolygonCollider2D>();
         BoxCollider2D[] box = GameObject.Find(mWorld.name + "/Doors").GetComponentsInChildren<BoxCollider2D>();
-        
-        // TODO si riesce a fixare?
-        Renderer r = GameObject.Find(mWorld.name + "/Background/water").GetComponent<Renderer>();
 
-        if (map == null || coll == null || r == null || poly == null)
+        // TODO si riesce a fixare?
+        //Renderer r = GameObject.Find(mWorld.name + "/Background/water").GetComponent<Renderer>();
+
+        if (map == null || coll == null || poly == null)
         {
             Debug.LogError("Cannot find map or colliders or 'water' layer in this map");
             return;
         }
 
-        mWidth = map.TileWidth;
-        mHeight = map.TileHeight;
-        mWorldArea = r.bounds;
-        mArea = new byte[mWidth * mHeight];
-        mEnemies = new byte[mWidth * mHeight];
+        InitializeMapMeasures(map);
 
-        for (int i = 0; i < mWidth * mHeight; i++)
+        mArea = new byte[mRowTiles * mColTiles];
+        mEnemies = new byte[mRowTiles * mColTiles];
+
+        for (int i = 0; i < mRowTiles * mColTiles; i++)
         {
             mArea[i] = 0;
             mEnemies[i] = 0;
         }
 
-        foreach(EdgeCollider2D c in coll)
+        foreach (EdgeCollider2D c in coll)
         {
             Vector2[] p = c.points;
             for (int i = 0; i < p.Length; i++)
@@ -165,11 +170,39 @@ public class AIMap : MonoBehaviour
         mMapRefreshes++;
     }
 
+    private void DrawLine(IntPoint s, IntPoint e)
+    {
+        if (s.y == e.y)
+        {
+            for (int x = s.x; x <= e.x; x++)
+                mArea[x + s.y * mRowTiles] = 1;
+            return;
+        }
+
+        if (s.x == e.x)
+        {
+            for (int y = s.y; y <= e.y; y++)
+                mArea[s.x + y * mRowTiles] = 1;
+            return;
+        }
+
+        float dy = e.y - s.y;
+        float dx = e.x - s.x;
+
+        float d = dy / dx;
+
+        for (int x = s.x; x <= e.x; x++)
+        {
+            float y = s.y + (x - s.x) * d;
+            mArea[x + (int)y * mRowTiles] = 1;
+        }
+    }
+
     private void BlackRect(IntPoint dl, IntPoint ur)
     {
         for (int x = dl.x; x <= ur.x; x++)
             for (int y = dl.y; y <= ur.y; y++)
-                mArea[x + y * mWidth] = 1;
+                mArea[x + y * mRowTiles] = 1;
     }
 
     private void BlackTri(IntPoint[] p)
@@ -188,59 +221,50 @@ public class AIMap : MonoBehaviour
     /* TODO MICHELE */
     private Vector2 TileToWorldCoordinates(IntPoint p)
     {
-        Vector2 ret;
-        float dx = mWorldArea.max.x - mWorldArea.min.x;
-        float dy = mWorldArea.max.y - mWorldArea.min.y;
-        float tsize = dx / mWidth / 2.0f;
+        Debug.Assert(p.x >= 0 && p.x < mRowTiles && p.y >= 0 && p.y < mColTiles, "Invalid tile coords: " + p.x + ", " + p.y);
 
-        ret.x = (((float)p.x / (mWidth-1)) * dx) + mWorldArea.min.x + tsize;
-        ret.y = (((float)p.y / (mHeight-1)) * dy) + mWorldArea.min.y + tsize;
-
-        //IntPoint test = toMap(ret);
-        //Debug.Log(string.Format("FromMap: {0},{1} -> {2},{3}; ToMap: {4},{5}", p.x, p.y, ret.x, ret.y, test.x, test.y));
-        return ret;
+        Vector2 result = new Vector2((p.x + 0.5f) * mTileWidthWC, (p.y + 0.5f) * mTileHeightWC);
+        Debug.Log(result);
+        return result;
     }
 
     private IntPoint WorldToTileCoordinates(Vector2 p)
     {
-        IntPoint ret;
-        float dx = mWorldArea.max.x - mWorldArea.min.x;
-        float dy = mWorldArea.max.y - mWorldArea.min.y;
-        ret.x = Mathf.FloorToInt(((p.x - mWorldArea.min.x) / dx) * (mWidth-1));
-        ret.y = Mathf.FloorToInt(((p.y - mWorldArea.min.y) / dy) * (mHeight-1));
+        IntPoint tileCoords;
 
-        if (ret.x >= mWidth) ret.x = mWidth - 1;
-        if (ret.y >= mHeight) ret.y = mHeight - 1;
-        if (ret.x < 0) ret.x = 0;
-        if (ret.y < 0) ret.y = 0;
-        return ret;
+        // find tile and clamp to limits
+        tileCoords.y = Mathf.FloorToInt(p.x / mTileWidthWC);
+        tileCoords.x = Mathf.FloorToInt((-p.y) / mTileHeightWC); // NB the minus sign!
+
+        tileCoords.y = Mathf.Clamp(tileCoords.y, 0, mRowTiles - 1);
+        tileCoords.x = Mathf.Clamp(tileCoords.x, 0, mColTiles - 1);
+
+        return tileCoords;
     }
 
-    private void DrawLine(IntPoint s, IntPoint e)
+    public byte[] GetMap()
     {
-        if (s.y == e.y)
-        {
-            for(int x = s.x; x <= e.x; x++)
-                mArea[x + s.y * mWidth] = 1;
-            return;
-        }
-
-        if (s.x == e.x)
-        {
-            for(int y = s.y; y <= e.y; y++)
-                mArea[s.x + y * mWidth] = 1;
-            return;
-        }
-
-        float dy = e.y - s.y;
-        float dx = e.x - s.x;
-
-        float d = dy / dx;
-
-        for (int x = s.x; x <= e.x; x++)
-        {
-            float y = s.y + (x - s.x) * d;
-            mArea[x + (int)y * mWidth] = 1;
-        }
+        return mArea;
     }
+
+    public byte[] GetEnemies()
+    {
+        return mEnemies;
+    }
+
+    public int GetMapRefreshId()
+    {
+        return mMapRefreshes;
+    }
+
+    public int GetWidth()
+    {
+        return mRowTiles;
+    }
+
+    public int GetHeight()
+    {
+        return mColTiles;
+    }
+
 }
