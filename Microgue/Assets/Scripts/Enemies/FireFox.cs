aea439f8429930231c5be5f155faf03a2b6d1112
MicheleAlbanese <michele.albanese93@gmail.com>
2017-01-03 23:18:12 +0100

    draft for firefox

--- Microgue/Assets/Scripts/Enemies/FireFox.cs
+++ Microgue/Assets/Scripts/Enemies/FireFox.cs
@@ -17,6 +17,8 @@ public class FireFox : MonoBehaviour {
 
     private EnemyPosition mEnemyPosition;
 
+    private Animator mAnimator;
+
     // STAGES
     // 1 - idle/rolling
     // 2 - idle/shooting/rolling
@@ -27,8 +29,20 @@ public class FireFox : MonoBehaviour {
     int[] rollingSpeeds = new int[] { 10, 20, 30 };
     int[] shootingSpeeds = new int[] { 5, 10 };
 
-    int cnt = 0;
-    int limit = 60;
+    Vector2[] targetPoints = new Vector2[]
+    {
+        new Vector2(3,-2), // up left
+        new Vector2(7,-2), // up right
+        new Vector2(3,-5), // bottom left
+        new Vector2(7,-5), // bottom right
+    };
+
+    // State machines
+    StateMachine<FireFox> mStateMachine;
+    static State<FireFox> mIdleState = new IdleState();
+    static State<FireFox> mChoosingDirState = new ChoosingDirState();
+    static State<FireFox> mRollingState = new RollingState();
+    static State<FireFox> mShootingState = new ShootingState();
 
     // Use this for initialization
     void Start () {
@@ -40,32 +54,120 @@ public class FireFox : MonoBehaviour {
         mEnemyPosition = GetComponent<EnemyPosition>();
         mEnemyPosition.SetEnabled(true);
 
-        state = FirefoxStates.IDLE;
+        mAnimator = GetComponent<Animator>();
+
+        mStateMachine = new StateMachine<FireFox>(this, mIdleState, null);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        ++cnt;
+        mStateMachine.Update();
+	}
 
-	    if( stage == 1 )
-        {
-            // update status
+    void FixedUpdate()
+    {
+        mStateMachine.FixedUpdate();
+    }
+
+    sealed class IdleState : State<FireFox>
+    {
+        public int limit = 60;
+        int cnt = 0;
+
+        public void Update(FireFox owner) {
+            ++cnt;
+
             if( cnt >= limit )
             {
-                if(state == FirefoxStates.IDLE)
-                {
-                    state = FirefoxStates.ROLLING;
-                } else
-                {
-                    state = FirefoxStates.IDLE;
-                }
+                cnt = 0;
+                owner.mStateMachine.ChangeState(FireFox.mChoosingDirState);
             }
-        } else if( stage == 2 )
-        {
+        }
 
-        } else
+        public void FixedUpdate(FireFox owner) {
+
+        }
+
+        public void OnEnter(FireFox owner)
+        {
+            owner.mAnimator.SetTrigger("idle");
+        }
+
+        public void OnExit(FireFox owner) { }
+    }
+
+    sealed class ChoosingDirState : State<FireFox>
+    {
+        public float mSpeed = 2.0f;
+
+        public void Update(FireFox owner)
+        {
+            
+        }
+
+        public void FixedUpdate(FireFox owner)
         {
 
         }
-	}
+
+        public void OnEnter(FireFox owner)
+        {
+            owner.mAnimator.SetTrigger("idle");
+            Rigidbody2D rb = owner.mRb;
+
+            Vector2 targetPos = new Vector2(Random.Range(3, 7), Random.Range(-5, -2));
+            rb.velocity = (rb.position - targetPos).normalized * mSpeed;
+
+            owner.mStateMachine.ChangeState(FireFox.mRollingState);
+        }
+
+        public void OnExit(FireFox owner) { }
+    }
+
+    sealed class RollingState : State<FireFox>
+    {
+        Rigidbody2D rb;
+        public int limit = 60;
+        int cnt = 0;
+
+        public void Update(FireFox owner) {
+            ++cnt;
+
+            if (cnt >= limit)
+            {
+                cnt = 0;
+                owner.mStateMachine.ChangeState(FireFox.mIdleState);
+            }
+        }
+
+        public void FixedUpdate(FireFox owner)
+        {
+           
+        }
+
+        public void OnEnter(FireFox owner) {
+            rb = owner.mRb;
+            owner.mAnimator.SetTrigger("rolling");
+        }
+
+        public void OnExit(FireFox owner) { }
+    }
+
+    sealed class ShootingState : State<FireFox>
+    {
+        public void Update(FireFox owner) { }
+
+        public void FixedUpdate(FireFox owner)
+        {
+
+        }
+
+        public void OnEnter(FireFox owner)
+        {
+            
+        }
+
+        public void OnExit(FireFox owner) { }
+    }
+
 }
