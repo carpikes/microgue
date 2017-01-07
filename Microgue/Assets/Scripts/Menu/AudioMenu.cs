198666daeff6efadcfd58492a330f52932848d5a
MicheleAlbanese <michele.albanese93@gmail.com>
2017-01-07 12:32:02 +0100

    menu ui added

---
+++ Microgue/Assets/Scripts/Menu/AudioMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMODUnity;
+
+public class AudioMenu : MonoBehaviour {
+
+    StudioEventEmitter[] emitters;
+
+    void Start()
+    {
+        emitters = GetComponents<StudioEventEmitter>();
+        Debug.Assert(emitters != null, "Audio for menu not working.");
+    }
+
+	public void MouseOver()
+    {
+        emitters[0].Play();
+    }
+
+    public void ConfirmChoice()
+    {
+        emitters[1].Play();
+    }
+}
